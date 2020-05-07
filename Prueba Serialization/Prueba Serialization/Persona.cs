16a37a83b90d748f71dd49f0a656c4cf34e4928b
mathiascordova <macordova@miuandes.cl>
2020-05-07 09:47:02 -0400

    Avance?

---
+++ Prueba Serialization/Prueba Serialization/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Serialization
+{
+    [Serializable]
+    public class Persona
+    {
+        private string Name;
+        private string Surname;
+        private int Age;
+
+        public Persona(string Name, string Surname, int Age)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+        }
+    }
+}
---
+++ Prueba Serialization/Prueba Serialization/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Prueba_Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> listPersona;
+
+            void Almacenar(Persona persona)
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream("PerSave.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, persona);
+                stream.Close();
+            }
+            void Cargar()
+            {
+
+            }
+            
+
+            while (true)
+            {
+                Console.WriteLine("1. Crear Persona" + Environment.NewLine + "2. Ver Personas" + Environment.NewLine + "x. Salir");
+                string choice = Console.ReadLine();
+
+                if (choice == "x"||choice == "X")
+                {
+                    Console.WriteLine("Bye");
+                    break;
+                }
+                else if (choice == "1")
+                {
+                    Console.Write("Dame Nombre: ");
+                    string Name = Console.ReadLine();
+                    Console.Write("Dame Apellido: ");
+                    string Surname = Console.ReadLine();
+                    Console.Write("Dame Edad: ");
+                    int Age = Int32.Parse(Console.ReadLine());
+                    Persona per = new Persona(Name,Surname,Age);
+                    
+                }
+            }
+
+        }
+    }
+}
